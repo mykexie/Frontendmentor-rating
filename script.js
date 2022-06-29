3c9d3982a440f05bd0652124be9fe16a0f80bafa
mykexie <87225918+mykexie@users.noreply.github.com>
2022-06-29 21:44:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const check = document.querySelectorAll('.rate');
+const subBtn = document.querySelector('.submit');
+// console.log(check.length);
+if (check.length > 0) {
+    check.forEach(el => {
+        el.addEventListener('click', () => {
+            resetLinks();
+            el.classList.add('active');
+
+        })
+    })
+}
+function resetLinks() {
+    check.forEach(el => {
+        el.classList.remove('active');
+    })
+}
+subBtn.addEventListener('click', subClick);
+function subClick() {
+    subBtn.classList.add('active');
+    document.location.href = '/submitted.html';
+}
